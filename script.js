cd7e42c6b78788879f2753f30691b72b9b273266
RohanRathod777 <rohangr7777@gmail.com>
2025-09-21 19:21:19 +0530

    Portfilio UI updated and everything changed

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-// Typing Effect
-const typing = document.querySelector(".typing");
-const text = ["Web Developer", "Designer", "Programmer"];
-let i = 0;
-let j = 0;
-let currentText = "";
-let letter = "";
+let menuIcon = document.querySelector("#menu-icon");
+let navBar = document.querySelector('.navbar');
 
-(function type() {
-  if (i === text.length) {
-    i = 0;
-  }
-  currentText = text[i];
-  letter = currentText.slice(0, ++j);
-
-  typing.textContent = letter;
-  if (letter.length === currentText.length) {
-    i++;
-    j = 0;
-    setTimeout(type, 1000);
-  } else {
-    setTimeout(type, 150);
-  }
-})();
+menuIcon.onclick = () => {
+      menuIcon.classList.toggle('bx-x');
+      navBar.classList.toggle('active');
+}
